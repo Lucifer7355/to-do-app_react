6e7cc6cfa74804b6634240ac95fbdf386ac5fa59
Ankit Kumar Srivastava <ankitviddya@gmail.com>
2022-09-29 18:37:01 +0530

    created input form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Input } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import InputForm from './components/InputForm';
+import Todo from './components/Todo';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    const localTodos = localStorage.getItem("todos")
+    if (localTodos) {
+      setTodos(JSON.parse(localTodos))
+    }
+  }, [])
+
+  const addTodos = async todo => {
+    setTodos([...todos, todo])
+  }
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+  const completedFlag = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
   return (
-    <div className="App">
-      hello
-    </div>
+    <Container type="fluid">
+      <h1>My Todo App</h1>
+      <InputForm addTodos={addTodos} />
+      <Todo todos={todos} completedFlag={completedFlag} />
+    </Container>
   );
 }
 
